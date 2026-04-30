2785394a076462643ef178b810d5df4ce1dbfdab
meng <yangyangmeng539@gmail.com>
2026-05-01 03:38:35 +0800

    Release dev-0.1.4 ring3 entry prototype

--- kernel/version.h
+++ kernel/version.h
@@ -2,8 +2,8 @@
 #define LINGJING_VERSION_H
 
 #define LINGJING_NAME "Lingjing OS"
-#define LINGJING_VERSION "dev-0.1.3"
-#define LINGJING_STAGE "user mode preparation prototype"
+#define LINGJING_VERSION "dev-0.1.4"
+#define LINGJING_STAGE "ring3 entry prototype"
 #define LINGJING_ARCH "i386"
 #define LINGJING_BOOT "multiboot + grub"
 #define LINGJING_INTENT_LAYER "enabled"
